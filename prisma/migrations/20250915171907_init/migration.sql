b3cdb71b8f88621fb7903f51b5f7b9c0fac6084d
Haider Ali <mhaiderali.dev@gmail.com>
2025-09-15 17:24:11 +0500

    feat: updat migrateion

--- prisma/migrations/20250915171907_init/migration.sql
+++ prisma/migrations/20250915171907_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `accounts` (
+CREATE TABLE `wallet_accounts` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
     `chain` VARCHAR(50) NOT NULL,
@@ -8,14 +8,14 @@ CREATE TABLE `accounts` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `accounts_userId_idx`(`userId`),
-    INDEX `accounts_chain_idx`(`chain`),
-    INDEX `accounts_address_idx`(`address`),
+    INDEX `wallet_accounts_userId_idx`(`userId`),
+    INDEX `wallet_accounts_chain_idx`(`chain`),
+    INDEX `wallet_accounts_address_idx`(`address`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `coins` (
+CREATE TABLE `wallet_coins` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `symbol` VARCHAR(50) NOT NULL,
@@ -28,14 +28,14 @@ CREATE TABLE `coins` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `coins_address_idx`(`address`),
-    INDEX `coins_chain_idx`(`chain`),
-    UNIQUE INDEX `coins_symbol_chain_key`(`symbol`, `chain`),
+    INDEX `wallet_coins_address_idx`(`address`),
+    INDEX `wallet_coins_chain_idx`(`chain`),
+    UNIQUE INDEX `wallet_coins_symbol_chain_key`(`symbol`, `chain`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `transactions` (
+CREATE TABLE `wallet_transactions` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
     `accountId` VARCHAR(191) NOT NULL,
@@ -50,20 +50,20 @@ CREATE TABLE `transactions` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `transactions_txHash_key`(`txHash`),
-    INDEX `transactions_fromAddress_idx`(`fromAddress`),
-    INDEX `transactions_toAddress_idx`(`toAddress`),
-    INDEX `transactions_txHash_idx`(`txHash`),
-    INDEX `transactions_chain_idx`(`chain`),
-    INDEX `transactions_blockNumber_idx`(`blockNumber`),
-    INDEX `transactions_userId_idx`(`userId`),
-    INDEX `transactions_accountId_idx`(`accountId`),
-    INDEX `transactions_coinId_idx`(`coinId`),
+    UNIQUE INDEX `wallet_transactions_txHash_key`(`txHash`),
+    INDEX `wallet_transactions_fromAddress_idx`(`fromAddress`),
+    INDEX `wallet_transactions_toAddress_idx`(`toAddress`),
+    INDEX `wallet_transactions_txHash_idx`(`txHash`),
+    INDEX `wallet_transactions_chain_idx`(`chain`),
+    INDEX `wallet_transactions_blockNumber_idx`(`blockNumber`),
+    INDEX `wallet_transactions_userId_idx`(`userId`),
+    INDEX `wallet_transactions_accountId_idx`(`accountId`),
+    INDEX `wallet_transactions_coinId_idx`(`coinId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `balances` (
+CREATE TABLE `wallet_balances` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
     `accountId` VARCHAR(191) NOT NULL,
@@ -72,21 +72,22 @@ CREATE TABLE `balances` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `balances_userId_idx`(`userId`),
-    INDEX `balances_accountId_idx`(`accountId`),
-    INDEX `balances_coinId_idx`(`coinId`),
-    UNIQUE INDEX `balances_userId_accountId_coinId_key`(`userId`, `accountId`, `coinId`),
+    INDEX `wallet_balances_userId_idx`(`userId`),
+    INDEX `wallet_balances_accountId_idx`(`accountId`),
+    INDEX `wallet_balances_coinId_idx`(`coinId`),
+    UNIQUE INDEX `wallet_balances_userId_accountId_coinId_key`(`userId`, `accountId`, `coinId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `transactions` ADD CONSTRAINT `transactions_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `accounts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `wallet_transactions` ADD CONSTRAINT `wallet_transactions_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `wallet_accounts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `transactions` ADD CONSTRAINT `transactions_coinId_fkey` FOREIGN KEY (`coinId`) REFERENCES `coins`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `wallet_transactions` ADD CONSTRAINT `wallet_transactions_coinId_fkey` FOREIGN KEY (`coinId`) REFERENCES `wallet_coins`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `balances` ADD CONSTRAINT `balances_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `accounts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `wallet_balances` ADD CONSTRAINT `wallet_balances_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `wallet_accounts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `balances` ADD CONSTRAINT `balances_coinId_fkey` FOREIGN KEY (`coinId`) REFERENCES `coins`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `wallet_balances` ADD CONSTRAINT `wallet_balances_coinId_fkey` FOREIGN KEY (`coinId`) REFERENCES `wallet_coins`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
