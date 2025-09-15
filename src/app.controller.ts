43d3eae427a8111a41d02eb43e45d02045f23e8e
Haider Ali <mhaiderali.dev@gmail.com>
2025-09-15 23:04:50 +0500

    chore: add test api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller} from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get()
+  getHello(): string {
+    return "Hello World!";
+  }
+
   // @Get('kv/lists')
   // async lists() {
   //   try {
